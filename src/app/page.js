62385217a36a70997ae045f94673ecdf12895ba7
sablexis <sabrina.a.sandy@gmail.com>
2024-02-03 18:03:59 -0400

    :sparkles: new game button works

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-'use client';
-
+'use client'
 import Image from "next/image";
 import Script from "next/script";
+import Link from "next/link";
 import { IconButton, PropTypes } from "@mui/material";
 import React from "react";
 import NewGame from "@/components/game";
@@ -9,37 +9,34 @@ import StartPage from "@/pages/Home";
 import { Dialog, DialogTitle, List, ListItem } from "@mui/material";
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { Button } from "@mui/base";
-import { useState } from "react";
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import  styles from './page.module.css';
+
 
 
 
 export default function Home() {
-  const [open, setOpen] = useState(true);
-
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+  
+  function spinUpGame(){
+    <Link href="/game"></Link>
+  }
 
   return(
-    <div>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-    {/* <Script src="https://kit.fontawesome.com/f7b7deab76.js" crossorigin="anonymous"></Script> */}
-    <Dialog open = {open}>
-      <DialogTitle align="center">Hot Seat:<br></br> The Drinking Game 🔥</DialogTitle>
-      <List>
-            <ListItem alignItems="flex-start">Designate who's up first and the following order of players</ListItem>
-            <ListItem alignItems="flex-start">The player on the hot seat picks who they'd like a question from</ListItem>
-            <ListItem alignItems="flex-start">The person who's been picked to ask a question asks their question, If the question is one you wish not to ask, you must drink and tap 'new card' to pull a new card from the pile</ListItem>
-            <ListItem alignItems="flex-start">If the hot seat player wishes not to answer they must drink</ListItem>
-      </List>
-    <IconButton color="custom"
-    onClick={handleClose}>
-    <HighlightOffIcon/>
-    </IconButton>
-    </Dialog>
-    <NewGame />
+    <section className={styles.Home}>
+      <div className="firstPage">
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      {/* <Script src="https://kit.fontawesome.com/f7b7deab76.js" crossorigin="anonymous"></Script> */}
+      <h2>hot seat: the game 🔥</h2>
+      <div className="newGameBtnDiv">
+      <Button variant="outlined" 
+      startIcon={<PlayCircleOutlineIcon />}>
+        New Game
+      </Button>
+      <Link href="/startAGame">New Game!</Link>
+      </div>
     </div>
+    </section>
+    
     )
 }
 
