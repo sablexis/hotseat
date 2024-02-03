5c60c74568cc4ed820fdef3a883e015ddd595a13
sablexis <sabrina.a.sandy@gmail.com>
2024-02-03 16:42:00 -0400

    authorization fundamentals built, changing code structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options";
 
-export default function Home(){
+export default async function StartPage(){
+    const session = await getServerSession(options);
+
     return(
         <div>
             Home
+            {session ? (<Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+                ) : (
+
+            <Link href="/api/auth/signin?callbackUrl=/"></Link> )
+            } 
         </div>
     )
 }
