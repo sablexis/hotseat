fefa9eb9d7c78e2d9b65ce75767e1fda3b3b73da
sablexis <sabrina.a.sandy@gmail.com>
2024-02-05 19:59:27 -0400

    :sparkles: FINALLLLLY IMPLMENETED USER AUTH

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,44 +1,13 @@
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials"
+import User from "@/app/models/User";
+import bcrypt from "bcrypt";
 import NextAuth from "next-auth/next";
 
 export const options = {
     secret: process.env.NEXT_AUTH_SECRET,
     providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'Credentials',
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            credentials: {
-              email: { label: "email", type: "text", placeholder: "jsmith@email.com" },
-              password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials, req) {
-              // You need to provide your own logic here that takes the credentials
-              // submitted and returns either a object representing a user or value
-              // that is false/null if the credentials are invalid.
-              // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-              // You can also use the `req` object to obtain additional parameters
-              // (i.e., the request IP address)
-              const res = await fetch("/api/auth/register", {
-                method: 'POST',
-                body: JSON.stringify(credentials),
-                headers: { "Content-Type": "application/json" }
-              })
-              const user = await res.json()
-        
-              // If no error and we have user data, return it
-              if (res.ok && user) {
-                return user
-              }
-              // Return null if user data could not be retrieved
-              return null
-            }
-          }),
 
         GitHubProvider({
             profile(profile){
@@ -73,19 +42,56 @@ export const options = {
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
         }),
-
-    ],
-    callbacks:{
-        async jwt({token, user}){
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+              email: {
+                label: "email:",
+                type: "text",
+                placeholder: "your-email",
+              },
+              password: {
+                label: "password:",
+                type: "password",
+                placeholder: "your-password",
+              },
+            },
+            async authorize(credentials) {
+              try {
+                const foundUser = await User.findOne({ email: credentials.email })
+                  .lean()
+                  .exec();
+      
+                if (foundUser) {
+                  console.log("User Exists");
+                  const match = await bcrypt.compare(
+                    credentials.password,
+                    foundUser.password
+                  );
+      
+                  if (match) {
+                    console.log("Good Pass");
+                    delete foundUser.password;
+      
+                    foundUser["role"] = "Unverified Email";
+                    return foundUser;
+                  }
+                }
+              } catch (error) {
+                console.log(error);
+              }
+              return null;
+            },
+          }),
+        ],
+        callbacks: {
+          async jwt({ token, user }) {
             if (user) token.role = user.role;
             return token;
-        },
-
-        async session({session, token}){
+          },
+          async session({ session, token }) {
             if (session?.user) session.user.role = token.role;
             return session;
+          },
         },
-    },
-}
-
-export default NextAuth(options)
+      };
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,121 +1,87 @@
-'use client'
+"use client";
 
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
+const RegisterForm = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
-export default  function SignUp() {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-
-    const email = data.get('email');
-    const password = data.get('password');
-
-    const response = await fetch('/api/auth/register',{
-        method: "POST",
-        headers:{"Content-Type": "application/json"},
-        body: JSON.stringify({email, password
-        })
-    })
-
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage("");
+    const res = await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      "content-type": "application/json",
+    });
+
+    if (!res.ok) {
+      const response = await res.json();
+      setErrorMessage(response.message);
+    } else {
+      router.refresh();
+      router.push("/");
+    }
+  };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+    <>
+      <form
+        onSubmit={handleSubmit}
+        method="post"
+        className="LoginFormInputForm"
+      >
+        <h1>Register</h1>
+        <label>username</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.name}
+          className="m-2 bg-slate-400 rounded"
+        />
+        <label>Email</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.email}
+          className="m-2 bg-slate-400 rounded"
+        />
+        <label>Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={handleChange}
+          required={true}
+          value={formData.password}
+          className="m-2 bg-slate-400 rounded"
+        />
+        <input
+          type="submit"
+          value="Sign up"
+          className="bg-blue-300 hover:bg-blue-100"
+        />
+      </form>
+      <p className="text-red-500">{errorMessage}</p>
+    </>
   );
-}
+};
+
+export default RegisterForm;
