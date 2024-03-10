8d302bece70ed64b68ffdea554e07b9933b04a84
sablexis <sabrina.a.sandy@gmail.com>
2024-03-10 17:17:26 -0300

    :card_file_box: updated all mongoose schema

--- src/app/models/Decks.js
+++ src/app/models/Decks.js
@@ -6,7 +6,7 @@ async function main() {
     mongoose.connect('mongodb://127.0.0.1:27017/hotseat');
     mongoose.Promise = global.Promise
 
-const decksSchema = new Schema({
+const decksSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
@@ -16,11 +16,14 @@ const decksSchema = new Schema({
     questions: [{body: String}]
 
 })
+
+const Decks =  mongoose.model('Decks', decksSchema);
+
 }
 
 // mongoose.connect(process.env.MONGO_URI)
 
 
-const Decks = mongoose.models.Decks || mongoose.model("Decks", decksSchema);
 
-export default Decks;
+
+
